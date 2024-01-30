9d6fcd115911da66c8f820516584ce390d5ae5cb
Your Name <you@example.com>
2024-01-30 09:12:25 +0000

    hira112

--- project_hirai/src/main.js
+++ project_hirai/src/main.js
@@ -3,18 +3,12 @@
 import Vue from 'vue';
 import App from './App.js';
 createApp(App).mount('#app');
-export const app = Vue.createApp({
+const app = Vue.createApp({
  data: () => ({
         message:'HELLO Vue.js!'
     })
 }) ;
+app.mout('#app');
 
-/*import App from './App';
-import HelloWorld from './components/HelloWorld.vue'
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}*/
+
 
