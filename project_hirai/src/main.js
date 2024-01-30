50da402fc4059828c227f49c2c6e77e161caa730
Your Name <you@example.com>
2024-01-30 06:17:07 +0000

    hira110

--- project_hirai/src/main.js
+++ project_hirai/src/main.js
@@ -1,17 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-createApp(App).mount('#app')
+/*global createApp*/
+/*global Vue*/
+import Vue from 'vue';
+import App from './App.js';
+createApp(App).mount('#app');
 export const app = Vue.createApp({
  data: () => ({
         message:'HELLO Vue.js!'
     })
-}) 
+}) ;
 
-/*import HelloWorld from './components/HelloWorld.vue'
+/*import App from './App';
+import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
     HelloWorld
   }
-}
-*/
+}*/
+
