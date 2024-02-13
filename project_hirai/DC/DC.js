c3c91a4ae5fcf4cf79f44026d1694cb16b5ac47c
Your Name <you@example.com>
2024-02-13 06:45:01 +0000

    hira396

--- project_hirai/DC/DC.js
+++ project_hirai/DC/DC.js
@@ -97,3 +97,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+function validateSelection() {
+    // ラジオボタンの要素を取得
+    var radioButton1 = document.getElementById("car");
+    var radioButton2 = document.getElementById("cho");
+    
+    // どちらのラジオボタンも選択されていない場合にアラートを表示
+    if (!radioButton1.checked && !radioButton2.checked) {
+        alert("種目を選択してください。");
+        return false; // 送信をキャンセル
+    }
+    return true; // 送信を許可
+}
