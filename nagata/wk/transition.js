1b0c15a158ab7e9b5f1071c508199708119204cf
EC2 Default User <ec2-user@ip-172-31-8-193.ap-northeast-1.compute.internal>
2024-02-25 15:49:36 +0000

    naga29

---
+++ nagata/wk/transition.js
@@ -0,0 +1,23 @@
+<!-- transition.js -->
+function pageStartTransition(){
+    //遷移先のURL
+    var url = 'start.html';
+    // ページ遷移
+    window.location.href = url;
+}
+
+function pageInputTransition(){
+    if (validateSelection()) {
+        //遷移先のURL
+         var url = 'input.html';
+        // ページ遷移
+         window.location.href = url;
+    }
+}
+
+function pageEndTransition(){
+    //遷移先のURL
+    var url = 'complete.html';
+    // ページ遷移
+    window.location.href = url;
+}
